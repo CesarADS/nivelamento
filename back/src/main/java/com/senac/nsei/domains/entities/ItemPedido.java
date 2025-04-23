584569198b1dc042af2aa7dec5638445c2b9d575
César <cesaraalexandrea@gmail.com>
2025-04-22 23:26:46 -0300

    removi imports desnecessarios

--- back/src/main/java/com/senac/nsei/domains/entities/ItemPedido.java
+++ back/src/main/java/com/senac/nsei/domains/entities/ItemPedido.java
@@ -1,7 +1,5 @@
 package com.senac.nsei.domains.entities;
 
-import jakarta.annotation.Generated;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
