a574558d454236948f74d4a7d3ce4ae548be13e8
CESAR AUGUSTO ALEXANDRE ALVES <cesar.alves@alunos.sc.senac.br>
2025-04-25 21:56:11 -0300

    cors back

--- back/src/main/java/com/senac/nsei/presentation/configurations/SecurityConfiguration.java
+++ back/src/main/java/com/senac/nsei/presentation/configurations/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.senac.nsei.presentation.configurations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,6 +21,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
--- back/src/main/java/com/senac/nsei/presentation/controllers/AuthController.java
+++ back/src/main/java/com/senac/nsei/presentation/controllers/AuthController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class AuthController {
 
     @PostMapping
     @Operation(summary = "Fazer login", description = "Retorna um token JWT para requisições a API caso o usuário seja válido.")
-    public ResponseEntity<?> login(LoginRequest loginRequest) {
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         var resultGerarToken = tokenService.gerarToken(loginRequest);
         return ResponseEntity.ok(resultGerarToken);
     }
